c24a42e24d6bd3cf67f5b1cdd4883ec849dfc5ac
Prajjwal98Dubey <prajjwaldubey003@gmail.com>
2024-02-14 00:23:57 +0530

    check vercel

--- src/components/RandomQuestion.js
+++ src/components/RandomQuestion.js
@@ -6,7 +6,7 @@ const RandomQuestion = ({index,ques}) => {
     // }
   return (
     <div className='flex justify-center p-4'>
-        <a href={ques[index]}><div className='flex justify-center items-center w-[50px] h-[50px] rounded-full bg-[#763BC6] hover:cursor-pointer hover:bg-[#6E49A0]' >
+        <a href={ques[index]}><div className='flex justify-center items-center w-[50px] h-[50px] rounded-full bg-red-500 hover:cursor-pointer hover:bg-[#6E49A0]' >
         <img src={LINK_ICON_IMG} alt="loading" className="w-[25px] h-[25px] " />
         </div></a>
     </div>
